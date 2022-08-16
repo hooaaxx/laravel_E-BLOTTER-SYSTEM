1c6b8b0f509c1f8cf7aec59c822d9f93508b8552
Michel jou Bonganay <mikel062795@gmail.com>
2022-08-16 15:37:32 +0800

    landing page commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -437,7 +437,7 @@
         @if (Route::has('login'))
             @auth
                 <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
-                    {{ Auth::user()->name }}
+                    Hi {{ Auth::user()->name }}
                 </h2>
                 <div class="w-full mb-4">
                     <div class="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
